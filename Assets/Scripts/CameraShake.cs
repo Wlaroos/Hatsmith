9b94d744db0ec1a4b779acf7248bdf494e139ef2
Mikey Bess <mikeybess27@gmail.com>
2026-07-03 01:23:03 -0500

    Init

---
+++ Assets/Scripts/CameraShake.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraShake : MonoBehaviour
+{
+    Coroutine _shakeRoutine = null;
+    Vector3 _originalPos = new Vector3(0,0,0);
+
+    public static CameraShake Instance { get; private set; }
+
+    private void Awake()
+    {
+        if(Instance != null && Instance != this)
+        {
+            Destroy(this);
+        }
+        else
+        {
+            Instance = this;
+        }
+    }
+
+    public void Shake(float duration, float magnitude)
+    {
+        if(_shakeRoutine != null)
+        {
+            transform.localPosition = _originalPos;
+            StopCoroutine(_shakeRoutine);
+        }
+        StartCoroutine(ShakeRoutine(duration, magnitude));
+    }
+
+    IEnumerator ShakeRoutine(float duration, float magnitude)
+    {
+        _originalPos = transform.localPosition;
+
+        float elapsedTime = 0;
+
+        while(elapsedTime < duration)
+        {
+            float x = Random.Range(-1f, 1f) * magnitude;
+            float y = Random.Range(-1f, 1f) * magnitude;
+
+            transform.localPosition = new Vector3(x, y, _originalPos.z);
+
+            elapsedTime += Time.deltaTime;
+
+            yield return null;
+        }
+
+        transform.localPosition = _originalPos;
+    }
+}
+
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,61 @@
+﻿using System.Diagnostics;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.Serialization;
+using Debug = System.Diagnostics.Debug;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    [SerializeField] private float _moveSpeed = 5f;
+    [SerializeField] private Sprite[] _sprites;
+
+    private Rigidbody2D _rb;
+    private SpriteRenderer _sr;
+    private Animator _anim;
+
+    private Vector2 _moveDirection;
+    
+    void Awake()
+    {
+        // Assigning Refs
+        _rb = GetComponent<Rigidbody2D>();
+        _sr = GetComponent<SpriteRenderer>();
+        _anim = GetComponent<Animator>();
+    }
+    
+    void Update()
+    {
+        // Getting Movement From Inputs
+        _moveDirection.x = Input.GetAxisRaw("Horizontal");
+        _moveDirection.y = Input.GetAxisRaw("Vertical");
+    }
+
+    private void FixedUpdate()
+    {
+        // Changing Direction Sprites
+        if (_moveDirection != Vector2.zero)
+        {
+            // switch (_moveDirection.x)
+            // {
+            //     case < 0:
+            //         _sr.sprite = _moveDirection.y > 0 ? _sprites[0] : _sprites[3];
+            //         break;
+            //     case > 0:
+            //         _sr.sprite = _moveDirection.y > 0 ? _sprites[2] : _sprites[5];
+            //         break;
+            //     default:
+            //         _sr.sprite = _moveDirection.y > 0 ? _sprites[1] : _sprites[4];
+            //         break;
+            // }
+        }
+
+        float mouseScreenX = Input.mousePosition.x;
+        _sr.flipX = mouseScreenX <= Screen.width * 0.5f;
+
+        // Actual Movement
+        _rb.MovePosition(_rb.position + _moveDirection * _moveSpeed * Time.fixedDeltaTime);
+
+        _anim.SetBool("IsMoving", _moveDirection != Vector2.zero);
+    }
+}
